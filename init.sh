290bf7fad5caa7749c0476cdec7f6cd33686ae0c
gangmuk <gangmuk@gmail.com>
2023-11-13 17:33:14 -0600

    Add vimrc file

--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
+# 
+mv vimrc_file ~/.vimrc
+
+# install
 sudo apt-get upgrade -y
 sudo apt-get update
 python get-pip.py
 sudo apt-get install python3-pip -y
 pip install -r requirements.txt
 pyinfra inventory.py deploy.py
+
+
+# config
+git config pull.rebase true
