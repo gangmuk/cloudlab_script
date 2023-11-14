28b874285ef2f0ef842e653b5126e6e83fac41ff
gangmuk <gangmuk@gmail.com>
2023-11-14 01:17:09 -0600

    Update scripts...

--- init.sh
+++ init.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-user_name=gangmuk
-project_dir=/users/${user_name}/projects/cloudlab_script
-
-cp vimrc_file ~/.vimrc
-
-# install
-sudo apt-get upgrade -y
-sudo apt-get update
-sudo apt-get install python3-pip -y
 pip install -r requirements.txt
-#pyinfra inventory.py deploy.py
-
-# config
-git config pull.rebase true
+pyinfra inventory.py deploy.py
