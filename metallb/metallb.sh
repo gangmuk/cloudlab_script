010e1b1f111b216d84b10c5c4523dbb5c65f4830
gangmuk <gangmuk@gmail.com>
2023-11-16 01:37:27 -0600

    Add metallb script

---
+++ metallb/metallb.sh
@@ -0,0 +1,13 @@
+# see what changes would be made, returns nonzero returncode if different
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+	sed -e "s/strictARP: false/strictARP: true/" | \
+	kubectl diff -f - -n kube-system
+
+# actually apply the changes, returns nonzero returncode on errors only
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+	sed -e "s/strictARP: false/strictARP: true/" | \
+	kubectl apply -f - -n kube-system
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.12/config/manifests/metallb-native.yaml
+
+kubectl apply -f configmap.yaml
