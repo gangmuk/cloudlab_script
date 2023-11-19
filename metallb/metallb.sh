97153bc6ccc4a4546adb5fae004181ede813d144
gangmuk <gangmuk@gmail.com>
2023-11-18 22:27:43 -0600

    Update metallb script. (still not working actually.. It was...)

--- metallb/metallb.sh
+++ metallb/metallb.sh
@@ -1,3 +1,5 @@
+kubectl delete -f https://raw.githubusercontent.com/metallb/metallb/v0.13.12/config/manifests/metallb-native.yaml
+
 # see what changes would be made, returns nonzero returncode if different
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
 	sed -e "s/strictARP: false/strictARP: true/" | \
@@ -8,6 +10,8 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 	sed -e "s/strictARP: false/strictARP: true/" | \
 	kubectl apply -f - -n kube-system
 
+kubectl apply -f namespace.yaml
+
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.12/config/manifests/metallb-native.yaml
 
-kubectl apply -f configmap.yaml
+kubectl apply -f ipaddresspool.yaml
