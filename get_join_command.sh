57e8c80b3dedf5b79fb4970c49f222beff8cdfc1
gangmuk <gangmuk@gmail.com>
2023-11-25 23:44:03 -0600

    Update get_join_command.sh for 24 hours only valid token case

--- get_join_command.sh
+++ get_join_command.sh
@@ -1,4 +1,23 @@
+'''
+A token is valid only for 24 hours.
+If you want to join a new worker node in 24 hours after you created the last token, you have to generate a new token.
+
+1. To generate a new token, run the following command in the master node.
+- kubeadm token generate
+
+2. To get join command, run the following command in the master node.
+- kubeadm token create [token] --print-join-command
+
+3. Copy the output command of step 2 and run this command in a new worker node that you want to join with sudo
+'''
 token=$(kubeadm token list | sed -n '2p' | awk '{print $1}')
+
+if [ $token == "" ]
+then
+    echo "The last token is expired."
+    echo "Run 'kubeadam token generate' to generate a new token"
+    exit
+fi
 cert_hash=$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt \
     | openssl rsa -pubin -outform der 2>/dev/null \
     | openssl dgst -sha256 -hex \
