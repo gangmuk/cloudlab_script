234bf148676461c3e403ac9adfa0eb2d0082f366
gangmuk <gangmuk@gmail.com>
2023-12-23 18:49:02 -0600

    Update script

--- install_python_pkg.sh
+++ install_python_pkg.sh
@@ -1,13 +1,21 @@
-sudo apt-get install pip -y
-
-pip install -r requirements.txt
-
+ip_file=servers.txt
+project_dir=/users/gangmuk/projects/cloudlab_script
 directory_to_add="/users/gangmuk/.local/bin"
+
+pssh -i -h ${ip_file} "sudo apt-get install pip -y"
+
+path_add="export PATH=\$PATH:$directory_to_add"
+pssh -i -h ${ip_file} "${path_add} >> ~/.bashrc"
+
+pssh -i -h ${ip_file} "source ~/.bashrc"
+
+pssh -i -h ${ip_file} "pip install -r ${project_dir}/requirements.txt"
+
 # Check if the directory exists
-if [ -d "$directory_to_add" ]; then
-    # Add the directory to the PATH in ~/.bashrc
-    echo "export PATH=\$PATH:$directory_to_add" >> ~/.bashrc
-    echo "Directory added to PATH. Restart your terminal or run 'source ~/.bashrc'."
-else
-    echo "Error: Directory does not exist."
-fi
+#if [ -d "$directory_to_add" ]; then
+#    # Add the directory to the PATH in ~/.bashrc
+#    echo "export PATH=\$PATH:$directory_to_add" >> ~/.bashrc
+#    echo "Directory added to PATH. Restart your terminal or run 'source ~/.bashrc'."
+#else
+#    echo "Error: Directory does not exist."
+#fi
