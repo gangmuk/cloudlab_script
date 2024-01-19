8b3a6816d785b01b8661b5edd633432d54f986bc
gangmuk <gangmuk@gmail.com>
2024-01-18 20:54:45 -0600

    Update script and README.md

---
+++ run_on_macbook.sh
@@ -0,0 +1,14 @@
+./ssh_copy.sh
+echo "./ssh_copy.sh"
+sleep 3
+
+./mkdir_gitclone.sh
+echo "./mkdir_gitclone.sh"
+sleep 3
+
+./create_hosts_detail.sh
+echo "./create_hosts_detail.sh"
+sleep 3
+
+./setup_vimrc_and_bashrc.sh
+echo "./setup_vimrc_and_bashrc.sh"
