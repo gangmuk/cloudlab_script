617e154c6d94b118e6b4bc4d20442c2e112b0e56
gangmuk <gangmuk@gmail.com>
2024-02-03 12:59:13 -0600

    Update pyinfra for zellij, wrk, go, tinygo

--- run_on_cloudlab_node0.sh
+++ run_on_cloudlab_node0.sh
@@ -1,10 +1,3 @@
 ./install_basic_pkg_in_node0.sh
 
 pyinfra inventory.py deploy.py
-
-# luasocket for wrk2
-sudo luarocks install luasocket
-
-# tinygo
-wget https://github.com/tinygo-org/tinygo/releases/download/v0.30.0/tinygo_0.30.0_amd64.deb
-sudo dpkg -i tinygo_0.30.0_amd64.deb
