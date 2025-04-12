beab5ac3fb67a181951c9153d8770bbab77389b5
gangmuk <gangmuk@gmail.com>
2025-04-12 00:08:47 -0700

    Added virtual env for basci pkg install
    Signed-off-by: gangmuk <gangmuk@gmail.com>

--- install_basic_pkg_in_node0.sh
+++ install_basic_pkg_in_node0.sh
@@ -4,6 +4,10 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install pip -y
 
+sudo apt install python3-full
+python3 -m venv ~/myenv
+source ~/myenv/bin/activate
+
 pip install -r /users/gangmuk/projects/cloudlab_script/requirements.txt
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!"
